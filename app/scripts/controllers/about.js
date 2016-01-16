38f06ea31b75939ca1a8d1c70e6373c2fbc07e02
Matthew Dalby <matthew.dalby@pfadmins-MacBook-Pro.local>
2016-01-15 18:50:55 -0800

    Initial push

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name travelnativeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the travelnativeApp
+ */
+angular.module('travelnativeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
