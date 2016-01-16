93df75ff551edc8db5980061759abd11f8199b46
Matthew Dalby <matthew.dalby@pfadmins-MacBook-Pro.local>
2016-01-15 19:42:50 -0800

    base scaffolding add

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name travelNativeUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the travelNativeUiApp
+ */
+angular.module('travelNativeUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
