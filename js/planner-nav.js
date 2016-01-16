4b027c325512cb50e05e2b4e40b6118928412369
Matthew Dalby <matthew.dalby@pfadmins-MacBook-Pro.local>
2016-01-16 09:38:49 -0800

    additional navigation updates

--- js/planner-nav.js
+++ js/planner-nav.js
@@ -5,22 +5,35 @@ jQuery(document).ready(function ($) {
 
 	$("#home-master-nav").click(function(){
 		console.log("Navigate to main");
+
+		parent.navigateToMainSection('home');
 	});
 
 	$("#budget-master-nav").click(function(){
 		console.log("Navigate to budget");
+
+		parent.navigateToMainSection('budget');
 	});
 
 	$("#todo-master-nav").click(function(){
 		console.log("Navigate to todo");
+
+		parent.navigateToMainSection('todo');
 	});
 
 	$("#profile-master-nav").click(function(){
 		console.log("Navigate to profile");
+
+		window.parent.navigateToMainSection('profile');
 	});
 
 	$("#itinerary-master-nav").click(function(){
 		console.log("Navigate to itinerary");
+
+		parent.navigateToMainSection('itinerary');
+
+		
+
 	});
 
 });
