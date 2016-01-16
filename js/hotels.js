f22a207d85bf155826096bf66d96504e45743297
Achintya Singhal <achintya.singhal@chicklet.co>
2016-01-16 14:45:59 -0800

    Added hotel js

---
+++ js/hotels.js
@@ -0,0 +1,20 @@
+function generateHotelList(cityname){
+	console.log("City name=>"+ cityname);
+	$.ajax({
+	     url: '/hotel?city='+cityname,
+	     	type: "GET"
+	     }).done(function (data) {
+			var htmlText = " <ul class=\"list-group\"><li href=\"#\" class=\"list-group-item active\">Hotels in "+ cityname + "</li>";
+			for (var result in data.results) {
+				htmlText = htmlText + 
+					"<li class=\"list-group-item \">"
+						+" <span class=\"badge\">" + data.results[result].rating + "*</span>"
+						+  data.results[result].name + 
+					"</li>";			;
+			}
+			htmlText = htmlText + "</ul>";
+		    console.log("htmlText " + htmlText);
+
+			$("#itinerary-right-most-info").html(htmlText);  
+	});
+}
