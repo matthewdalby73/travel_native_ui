177f80016764aedb9a5599a3b9fb14a5fa4dd518
Matthew Dalby <matthew.dalby@pfadmins-MacBook-Pro.local>
2016-01-16 12:56:53 -0800

    ui updates

--- js/sections/itinerary.js
+++ js/sections/itinerary.js
@@ -4,6 +4,28 @@ console.log("intinerary.js loaded");
 
 function hotelDetailSelected(){
 	console.log("hotelDetailSelected()");
+
+	/*
+	$.get('/html/partials/hotels_selection.html', function (data) {
+	    var template=Handlebars.compile(data);
+	    $("#planner-detail-panel".html(template(jsonData));
+	}, 'html')
+	*/
+
+	hotelData = {};
+
+	 $.ajax({
+                url: '/html/partials/hotels_selection.html',
+                cache: false,
+                success: function (data) {
+                    source = data;
+                    template = Handlebars.compile(source);
+                    $('#planner-detail-panel').html(template(hotelData));
+                    console.dir($('#planner-detail-panel'));
+
+                }
+            });   
+
 }
 
 function restaurantDetailSelected(){
@@ -13,3 +35,4 @@ function restaurantDetailSelected(){
 function pointOfInterestDetailSelected(){
 	console.log("pointOfInterestDetailSelected()");
 }  
+
