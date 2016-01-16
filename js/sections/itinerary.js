9b78702abb830b4826a0cdcbe2b03d19428b077d
Parker Lewis <parker.lewis14@att.net>
2016-01-16 13:57:50 -0800

    modularized the code

--- js/sections/itinerary.js
+++ js/sections/itinerary.js
@@ -35,37 +35,6 @@ function loadHotelSelectionPanel(hotelData){
             });   
 }
 
-function restaurantDetailSelected(){
-
-	// data = {"restaurants" : [{"name":"restaurant 1"},{"name":"restaurant 2"}]};
-	console.log("sdsa");
-	$.get("http://localhost:3000/restaurant?city=modesto.json", function(data){
-		// console.log(data)
-		restaurants = []
-		for (var i = data.results.length - 1; i >= 0; i--) {
-			restaurants.push({"name": data.results[i].name})
-		};
-		console.log(restaurants)
-		data = {"restaurants" : restaurants}
-		loadRestaurantData(data)
-	})
-
-}
-
-function loadRestaurantData(restaurantData){
-	$.ajax({
-                url: '/html/partials/restaurants_selection.html',
-                cache: false,
-                success: function (data) {
-                		console.log("121212121212");
-                		console.log(data);
-                    source = data;
-                    template = Handlebars.compile(source);
-                    $('#planner-detail-panel').html(template(restaurantData));
-
-                }
-            });  
-}
 
 function pointOfInterestDetailSelected(){
 	console.log("pointOfInterestDetailSelected()");
@@ -77,19 +46,7 @@ function pointOfInterestDetailSelected(){
 	loadPointOfInterestData(data);
 }  
 
-function getRestaurants(city) {
-	$.get({url: "http://localhost:3000/restaurant?city=" + city, dataType: "application/json", success: displayRestaurants(data)})
-};
 
-function displayRetaurants(data){
-	var out = "<ul>";
-
-	for(var i=0, l=data.results.length; i<l; i++) {
-		out = out + "<li>" + data.results[i].name + "</li>";
-	}
-
-	$("#restaurants_div").html( out + "</ul>");
-}
 function loadPointOfInterestData(poiData){
 	$.ajax({
                 url: '/html/partials/poi_selection.html',
