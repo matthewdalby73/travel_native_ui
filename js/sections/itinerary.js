f44b39f4ee6cdd61c42033efd75d33a525c7a06b
Matthew Dalby <matthew.dalby@pfadmins-MacBook-Pro.local>
2016-01-16 12:00:37 -0800

    ui updates

---
+++ js/sections/itinerary.js
@@ -0,0 +1,15 @@
+
+
+console.log("intinerary.js loaded");
+
+function hotelDetailSelected(){
+	console.log("hotelDetailSelected()");
+}
+
+function restaurantDetailSelected(){
+	console.log("restaurantDetailSelected()");
+}
+
+function pointOfInterestDetailSelected(){
+	console.log("pointOfInterestDetailSelected()");
+}  
